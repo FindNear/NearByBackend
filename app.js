9f549478d98da79e5ad4b2053dcbcf043a24bc4d
asasanur <adityasasanur@gmail.com>
2022-08-09 15:47:16 -0700

    create intermediary possibleLocations array for places

--- app.js
+++ app.js
@@ -19,24 +19,29 @@ app.get('/', (req, res) => {
 app.get('/:lat/:long/:mode/:time', (req,res) => {
     var location = req.params.lat + "%2C" + req.params.long
     var radius = speeds[req.params.mode] * req.params.time * 60
+
     if (req.query.type) {
         var type = req.query.type
     } else {
         var type = "restaurant"
     }
+
+    var possibleLocations = {}
     var config = {
         method: 'get',
         url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ location + '&radius=' + radius + '&type=' + type + '&key=' + apiKey,
         headers: { }
     };
-
     axios(config)
     .then(function (response) {
-        res.send(JSON.stringify(response.data));
+        possibleLocations = response.data.results;
+        res.send(JSON.stringify(possibleLocations));
     })
     .catch(function (error) {
         console.log(error);
     });
+
+
 })
 
 app.listen(port);
